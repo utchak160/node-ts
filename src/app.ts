c0dd3c8aaa180207673e2ee16a7dda8a1bb840d8
utchak160 <chakravartyutkarsh@gmail.com>
2020-09-04 15:49:33 +0530

    strategy update

--- src/app.ts
+++ src/app.ts
@@ -35,7 +35,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 
-
+require('./config/facebook');
 passport.serializeUser((user, done) => {
     done(null, user)
 });
